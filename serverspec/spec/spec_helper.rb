eee0ad6ec477d68b271c609cc58233f91029cd3c
saki nemoto <sk.nmt8@gmail.com>
2025-04-06 09:39:52 +0900

    modify

--- serverspec/spec/spec_helper.rb
+++ serverspec/spec/spec_helper.rb
@@ -2,8 +2,13 @@ require 'serverspec'
 require 'net/ssh'
 
 set :backend, :ssh
-host = ENV['AWS_EC2_PUBLIC']
-options = Net::SSH::Config.for(host)
 
+host = ENV['TARGET_HOST']  
+raise "TARGET_HOST is not set!" unless host
+
+options = Net::SSH::Config.for(host)
+options[:user] = 'ec2-user'
+options[:keys] = ['~/.ssh/id_rsa']
 set :host, host
 set :ssh_options, options
+set :disable_sudo, false
