3e7a8db753625d2308bc3878a3b2eaa8726e621d
saki nemoto <sk.nmt8@gmail.com>
2025-04-06 20:16:53 +0900

    modify

--- serverspec/spec/spec_helper.rb
+++ serverspec/spec/spec_helper.rb
@@ -3,8 +3,8 @@ require 'net/ssh'
 
 set :backend, :ssh
 
-host = ENV['TARGET_HOST']  
-raise "TARGET_HOST is not set!" unless host
+host = ENV['AWS_EC2_PUBLIC']  
+raise "AWS_EC2_PUBLIC is not set!" unless host
 
 options = Net::SSH::Config.for(host)
 options[:user] = 'ec2-user'
