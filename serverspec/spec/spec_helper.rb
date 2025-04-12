437e89b033a1b40ff64b6da9826b4d510a7e5a56
saki nemoto <sk.nmt8@gmail.com>
2025-04-12 23:58:34 +0900

    modify

--- serverspec/spec/spec_helper.rb
+++ serverspec/spec/spec_helper.rb
@@ -7,10 +7,7 @@ host = ENV['AWS_EC2_PUBLIC']
 raise "AWS_EC2_PUBLIC is not set!" unless host
 
 options = Net::SSH::Config.for(host)
-options[:user] = 'ec2-user'
-options[:keys] = [File.expand_path('~/.ssh/id_rsa')]
-options[:auth_methods] = ['publickey']                     # ← 明示追加
-options[:verify_host_key] = :never                         # ← 必須！
-set :host, host
+options[:user] ||= "ec2-user"
+set :host,        options[:host_name] || host
 set :ssh_options, options
 set :disable_sudo, false
